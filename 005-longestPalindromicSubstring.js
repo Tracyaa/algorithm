fe6507e5982d5e5c0c4b527f326b6832dad8c5ae
Tracyaa <tracyho0867@gmail.com>
2019-08-04 17:17:47 -0400

    print out al the possible ways

---
+++ 005-longestPalindromicSubstring.js
@@ -0,0 +1,23 @@
+// Given a string s, find the longest palindromic substring in s. 
+// You may assume that the maximum length of s is 1000.
+
+var longestPalindrome = function(s) {
+     // use obj to do word count 
+     let obj = {}; 
+     let arr = s.split("")
+     
+     for (let i = 0; i < arr.length - 1; i++) {
+         for (let j = i + 2; j < arr.length + 1; j++) {
+             let currentStr = arr.slice(i, j)
+             if (currentStr === currentStr.reverse()) {
+                 console.log(currentStr, currentStr.reverse())
+                 obj[currentStr.length] = currentStr.join("")
+             }
+       }
+     }
+     console.log(obj)
+     
+     const lastKey = Object.keys(obj).length - 1
+     
+     return obj[lastKey]
+};
