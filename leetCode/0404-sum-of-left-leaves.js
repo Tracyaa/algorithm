4a11b47de43d75c54ed6bfd33e009aea13f390d5
Tracyaa <tracyho0867@gmail.com>
2019-08-25 18:26:19 -0400

    setup variable

--- leetCode/0404-sum-of-left-leaves.js
+++ leetCode/0404-sum-of-left-leaves.js
@@ -10,7 +10,25 @@
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
-   
+    let sum = 0
+    
+    function sumLeft(node) {
+        if (!node) {
+            return;
+        }
+        
+        sumLeft(node.right);  
+        
+        if (node.left && !node.left.left && !node.left.right) {
+            sum += node.left.val;
+        }
+        
+        sumLeft(node.left);
+    };
+    
+    sumLeft(root);
+    
+    return sum;
 };
 
 
