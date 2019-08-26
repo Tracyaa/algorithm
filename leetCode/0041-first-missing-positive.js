ebff8c961ea7ef7121f3cbd88b39df0c0436391e
Tracyaa <tracyho0867@gmail.com>
2019-08-25 21:14:58 -0400

    edge case

--- leetCode/0041-first-missing-positive.js
+++ leetCode/0041-first-missing-positive.js
@@ -1,3 +1,11 @@
+
+// Given an unsorted integer array, find the smallest missing positive integer.
+
 var firstMissingPositive = function(nums) {
-   
+    // to eliminate the edge case
+    if (nums.length < 1) {
+        return 1
+    }
+
+    
 };
