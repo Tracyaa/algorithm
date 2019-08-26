62f4a7096eb5db69445b16d82213c2c502400435
Tracyaa <tracyho0867@gmail.com>
2019-08-25 22:07:57 -0400

    0041 solution

--- leetCode/0041-first-missing-positive.js
+++ leetCode/0041-first-missing-positive.js
@@ -2,10 +2,34 @@
 // Given an unsorted integer array, find the smallest missing positive integer.
 
 var firstMissingPositive = function(nums) {
-    // to eliminate the edge case
-    if (nums.length < 1) {
-        return 1
-    }
-
+      // eliminate the edge case 
+      if (nums.length < 1) {
+        return 1;
+    };
     
+    // we can use hash/obj to sort the numbers array
+    let myHash = {}; 
+    
+    // we will take off negative numbers when we create myHash
+    for (let num of nums) {if (num > 0) myHash[num] = true};
+    
+    // myHash = { '0': true, '1': true, '2': true }
+    
+    // 1 is first positive integer
+    let missing = 1; 
+    
+    
+    for (missing; missing <= nums.length; missing++) {
+        
+        // if the [missing key] doesn't exit, then we return the number
+        if (!myHash[missing]) {
+            return missing;
+        };
+        
+        // else we increment 1 then keep looping
+    };
+    
+    // otherwise return 1 
+    return missing; 
+
 };
