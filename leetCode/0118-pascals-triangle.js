fe19fd2ef9ab3dc59d33822c720ca6372a19856a
Tracyaa <tracyho0867@gmail.com>
2019-09-01 17:56:01 -0400

    function create nextRwo

--- leetCode/0118-pascals-triangle.js
+++ leetCode/0118-pascals-triangle.js
@@ -6,9 +6,20 @@ var generate = function(numRows) {
     // we need the base and it will be able to build up
     let arr = [[1], [1, 1]];
 
-    function nextRwo(nums) {
+    // making a helper function to create the next row
+    function nextRow(nums) {
+        // the first number is always 1 
+        let newArr = [1];
+        
+        for (let i = 0; i < nums.length - 1; i++) {
+            newArr.push(nums[i]+nums[i+1]);
+        };
 
-    }
+        // the last number is always 1 
+        newArr.push(1);
+        return newArr;
+    };
+
 
     return arr;
 };
