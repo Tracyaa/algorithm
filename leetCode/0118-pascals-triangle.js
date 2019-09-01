01e0bfe20b05121b9ab0579786d5a5838068cc59
Tracyaa <tracyho0867@gmail.com>
2019-09-01 17:10:32 -0400

    setting up problem

---
+++ leetCode/0118-pascals-triangle.js
@@ -0,0 +1,7 @@
+// Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+// image address: https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif
+// In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+var generate = function(numRows) {
+    
+};
